42ffa1dea7eec323c4187a7d23bf4ed7058f2657
Sindre Stephansen <sindre@sindrestephansen.com>
2016-08-10 20:23:03 +0200

    Added pancurses for a nicer visual representation.

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,147 @@
 extern crate chess;
+extern crate pancurses;
 
-use std::io::{self, Write};
 use chess::*;
+use pancurses::*;
 
 fn main() {
-    println!("Welcome to command line chess. Execute a move by typing in the algebraic notation for the move and hitting Return.");
+    let mut window = pancurses::initscr();
     let mut game = Game::new();
     let mut color = Color::Black;
+    let mut turn = 0;
+    let mut base: String = String::new();
     let mut input: String;
+    let mut invalid: bool;
+    let mut proper: String;
+    let mut buffer: Vec<Option<String>> = vec![Some("Welcome to command line chess. \
+        Execute a move by typing in the algebraic notation for the move and hitting Return.".to_string()), None, None];
+
+    pancurses::noecho();
+    window.nodelay(false);
 
     loop {
         color = match color {
             Color::White => Color::Black,
-            Color::Black => Color::White,
+            Color::Black => {
+                turn += 1;
+                Color::White
+            },
         };
+        if color == Color::White {
+            base = format!("{}: ", turn);
+        }
 
+        invalid = false;
         loop {
             input = String::new();
-            print!("{}: ", color);
-            io::stdout().flush().unwrap();
-            io::stdin().read_line(&mut input).expect("Failed to read line.");
+            buffer.push(None);
+            loop {
+                buffer.pop();
+                let mut s = base.clone();
+                s.push_str(&input);
+                buffer.push(Some(s));
+                draw(&mut window, &buffer, None);
+                if let Some(inp) = window.getch() {
+                    if let Input::Character(ch) = inp {
+                        match ch {
+                            'a' ... 'z' | 'A' ... 'Z' | '0' ... '9' => {
+                                input.push(ch);
+                            },
+                            '\u{0008}' => {input.pop();},
+                            '\n' | '\t' | ' ' => break,
+                            _ => panic!("Invalid input '{}'", ch),
+                        }
+                    }
+                }
+            }
+
             match game.an_to_move(&input.trim(), color) {
                 Some(v) => {
+                    if invalid {
+                        let len = buffer.len();
+                        buffer.remove(len-2);
+                    }
+
+                    proper = game.move_to_an(&v, true);
+                    buffer.pop();
+                    if color == Color::Black {
+                        let mut s = base.clone();
+                        s.push_str(&proper);
+                        buffer.push(Some(s));
+                    } else {
+                        base.push_str(&proper);
+                        for _ in base.len()..15 {
+                            base.push(' ');
+                        }
+                    }
+
                     game.move_pieces(&v);
                     break;
                 },
-                None => println!("Invalid move"),
-            }
+                None => {
+                    if invalid {
+                        buffer.pop();
+                    }
+                    invalid = true;
+                    buffer.pop();
+                    buffer.push(Some("Invalid move".to_string()));
+                },
+            };
         }
 
         if let Some(v) = game.check_victory() {
-            match v.0 {
-                Victory::Checkmate => println!("{} won by checkmate!", v.1),
-                Victory::Stalemate => println!("Stalemate"),
-                Victory::Draw      => println!("Draw"),
-            }
+            buffer.push(None);
+            buffer.push(Some(match v.0 {
+                Victory::Checkmate => {
+                    let mut s = String::new();
+                    s.push_str(match v.1 {
+                        Color::White => "White",
+                        Color::Black => "Black",
+                    });
+                    s.push_str(" won by checkmate!");
+                    s
+                },
+                Victory::Stalemate => "Stalemate".to_string(),
+                Victory::Draw      => "Draw".to_string(),
+            }));
+
+            buffer.push(None);
+            buffer.push(Some("Press any key to quit.".to_string()));
+
+            draw(&mut window, &buffer, None);
             break;
         }
     }
+
+    window.getch();
+
+    pancurses::delwin(window);
+    pancurses::endwin();
+}
+
+fn draw(window: &mut pancurses::Window, buffer: &[Option<String>], index: Option<usize>) {
+    let len = buffer.len();
+    let mut height: usize = 0;
+    for _ in 0..window.get_max_y() {
+        height += 1;
+    }
+
+    let start: usize = if let Some(v) = index {
+        v
+    } else if len > height {
+        len - height
+    } else {
+        0
+    };
+
+    window.erase();
+    for i in start..len {
+        if let Some(ref v) = buffer[i] {
+            window.addstr(v);
+        }
+        if i != len - 1 {
+            window.addch('\n');
+        }
+    }
+    window.refresh();
 }
